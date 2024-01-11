6b3854c1e4d606d768ef914e8a4d7caae17f9e62
Gianluca Guida <gianluca@rivosinc.com>
2024-01-11 18:47:39 +0000

    zabha poc: enable queue spinlock if zabha is supported

---
+++ arch/riscv/include/asm/mcs_spinlock.h
@@ -0,0 +1,6 @@
+#ifndef _ASM_RISCV_MCS_SPINLOCK_H
+#define _ASM_RISCV_MCS_SPINLOCK_H
+
+#include <asm-generic/mcs_spinlock.h>
+
+#endif /* _ASM_RISCV_MCS_SPINLOCK_H */
---
+++ arch/riscv/include/asm/qspinlock_types.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_RISCV_QSPINLOCK_TYPES_H
+#define _ASM_RISCV_QSPINLOCK_TYPES_H
+
+#include <asm-generic/qspinlock_types.h>
+
+#endif
